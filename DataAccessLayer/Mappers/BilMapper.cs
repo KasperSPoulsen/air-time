9a18fb0432c85e57f2779fbd72ac54b7efd65524
Clemen <eaa23clda@students.eaaa.dk>
2025-04-24 10:26:52 +0200

    Startet på at lave DB logik

---
+++ DataAccessLayer/Mappers/BilMapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Mappers
+{
+    internal class BilMapper
+    {
+        // Mapper fra DAL til DTO
+        public static DTO.Model.Bil Map(DAL.Model.Bil bil)
+        {
+            return new DTO.Model.Bil(
+                
+            );
+        }
+
+        // Mapper fra DTO til DAL
+        public static DAL.Model.Bil Map(DTO.Model.Bil bil)
+        {
+            return new DAL.Model.Bil(
+                
+            );
+        }
+    }
+}
