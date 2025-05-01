975f8f58d299af2d5164f7f99c24c93f1e30454b
Taus <dettavseunivers@gmail.com>
2025-05-01 12:52:58 +0200

    Tilføjet næsten al traening funktionalitet. *MANGLER TILFØJELSE I FRONTEND*

---
+++ DataAccessLayer/Repositories/TraeningRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Context;
+using DataAccessLayer.Mappers;
+using DataTransferObject.Model;
+
+namespace DataAccessLayer.Repositories
+{
+    public class TraeningRepository
+    {
+        // Så vi kan få en træning
+        public static Traening GetTraening(int id)
+        {
+            using (AirTimeContext context = new AirTimeContext())
+            {
+                return TraeningMapper.Map(context.Traeninger.Find(id));
+            }
+        }
+        // Liste af alle træninger
+        public static List<Traening> GetAlleTraeninger()
+        {
+            using(AirTimeContext context = new AirTimeContext())
+            {
+                return context.Traeninger.ToList().Select(TraeningMapper.Map).ToList();
+            }
+        }
+
+        public static void AddTraening(Traening traening)
+        {
+            using (AirTimeContext context = new AirTimeContext())
+            {
+                context.Traeninger.Add(TraeningMapper.Map(traening));
+                context.SaveChanges();
+            }
+        }
+    }
+}
