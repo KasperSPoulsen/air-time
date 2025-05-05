9dd17cde9824dd551d4ebecf3fac4e5a4ff3d8ff
Clemen <eaa23clda@students.eaaa.dk>
2025-05-05 19:06:33 +0200

    Opdatering til at hente springer fra db

--- DataTransferObject/Model/Springer.cs
+++ DataTransferObject/Model/Springer.cs
@@ -1,33 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace DataTransferObject.Model
 {
     public class Springer
     {
-
         public int Id { get; set; }
         public string Navn { get; set; }
-        public List<string> KonkurrenceSerie { get; set; }
+        public virtual List<string> KonkurrenceSerie { get; set; } = new List<string>();
         public DateTime? Foedselsdato { get; set; }
         public string TraeningsMaal { get; set; }
-        public KontaktPerson KontaktPerson { get; set; }
-        public List<Hold> Hold { get; set; } = new List<Hold>();
-
-
+        public virtual KontaktPerson KontaktPerson { get; set; }
+        public virtual List<Hold> Hold { get; set; } = new List<Hold>();
         public Springer()
         {
         }
         public Springer(string navn, DateTime? foedselsdato, KontaktPerson kontaktPerson, List<Hold> hold)
         {
-            
             Navn = navn;
             Foedselsdato = foedselsdato;
             KontaktPerson = kontaktPerson;
             Hold = hold;
+
         }
 
         public Springer(string navn, DateTime? foedselsdato, KontaktPerson kontaktPerson)
@@ -36,7 +31,7 @@ namespace DataTransferObject.Model
             Navn = navn;
             Foedselsdato = foedselsdato;
             KontaktPerson = kontaktPerson;
-            
+
         }
     }
 }
--- WpfApp1/SpringerInfo.xaml.cs
+++ WpfApp1/SpringerInfo.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,8 +21,21 @@ namespace WpfApp1
     /// Interaction logic for SpringerInfo.xaml
     /// </summary>
 
-public partial class SpringerInfo : Window
+public partial class SpringerInfo : Window, INotifyPropertyChanged
     {
+
+        public ObservableCollection<Springer> Springer { get; set; }
+
+        private Springer _selectedSpringer;
+        public Springer SelectedSpringer
+        {
+            get => _selectedSpringer;
+            set
+            {
+                _selectedSpringer = value;
+                OnPropertyChanged(nameof(SelectedSpringer));
+            }
+        }
         private Springer _springer;
 
         public SpringerInfo(Springer springer)
@@ -35,18 +50,40 @@ public partial class SpringerInfo : Window
         {
             NavnLabel.Content = springer.Navn ?? "";
             FødselsdagLabel.Content = springer.Foedselsdato?.ToString("dd-MM-yyyy") ?? "";
-            HoldLabel.Content = string.Join(", ", springer.Hold?.Select(h => h.Navn) ?? new List<string>());
             TræningsmålTekst.Text = springer.TraeningsMaal ?? "";
+            HoldLabel.Content = string.Join(", ", springer.Hold?.Select(h => h.HoldNavn) ?? new List<string>());
+
 
             if (springer.KontaktPerson != null)
             {
                 KontaktNavnLabel.Content = springer.KontaktPerson.Navn ?? "";
-                KontaktTelefonLabel.Content = springer.KontaktPerson.Telefonnummer ?? "";
-                KontaktEmailLabel.Content = springer.KontaktPerson.Email ?? "";
+                KontaktTelefonLabel.Content = springer.KontaktPerson.TlfNr ?? "";
+                KontaktEmailLabel.Content = springer.KontaktPerson.Mail ?? "";
             }
 
             // Hvis du har springserier, sæt dem her:
-            SpringserierListe.ItemsSource = springer.KonkurrenceSerie;
+            KonkurrenceserierListe.ItemsSource = springer.KonkurrenceSerie;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+        private void OpdaterSpringer_Click(object sender, RoutedEventArgs e)
+        {
+            
+                var edit = new EditSpringer(_springer);
+                edit.Closed += Edit_Dialog_Closed;
+                //mærk mærke til denne EventHandler – bruges senere når edit vinduet er lukket
+                edit.ShowDialog();
+            
+        }
+
+        private void Edit_Dialog_Closed(object sender, EventArgs e)
+        {
+            VisSpringerInfo(_springer); // sørger for UI opdateres
         }
     }
 
