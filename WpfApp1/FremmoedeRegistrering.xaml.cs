d13554abade9353a2e7e27739b0d874cc680ca59
Dennis <eaa23ddh@students.eaaa.dk>
2025-05-08 14:31:26 +0200

    Duplicate fixed på fremmøderegistrering ´

--- WpfApp1/FremmoedeRegistrering.xaml.cs
+++ WpfApp1/FremmoedeRegistrering.xaml.cs
@@ -74,7 +74,7 @@ namespace WpfApp1
 
                 var statusBTN = new Button
                 {
-                    Content = "SYG",
+                    Content = "FREMMØDT",
                     Tag = springer,
                     VerticalAlignment = VerticalAlignment.Center,
                     Margin = new Thickness(10, 0, 0, 0)
@@ -101,8 +101,11 @@ namespace WpfApp1
                 case "FRAVÆRENDE":
                     button.Content = "FREMMØDT";
                     break;
-                default:
+                case "FREMMØDT":
                     button.Content = "SYG";
+                    break;
+                default:
+                    button.Content = "FREMMØDT";
                     break;
             }
         }
@@ -116,7 +119,7 @@ namespace WpfApp1
             }
 
             DateTime valgtDato = DatoVælger.SelectedDate.Value;
-
+            Console.WriteLine("Antal children" + PlayerStackPanel.Children.Count);
             foreach (StackPanel stack in PlayerStackPanel.Children)
             {
                 var button = stack.Children.OfType<Button>().FirstOrDefault();
@@ -135,6 +138,8 @@ namespace WpfApp1
                             status = Status.FRAVAERENDE;
                             break;
                         case "SYG":
+                            status = Status.SYG;
+                            break;
                         default:
                             status = Status.FREMMOEDT;
                             break;
@@ -146,13 +151,15 @@ namespace WpfApp1
                         MoedeStatus = status
                     };
 
-                    FremmoederegistreringBLL.AddFremmoederegistrering(fremmoede);
+                    //FremmoederegistreringBLL.AddFremmoederegistrering(fremmoede);
                     alleFremmoederegistreringer.Add(fremmoede);
                     
 
                 }
+                Console.WriteLine("Antal children" + PlayerStackPanel.Children.Count);
+
             }
-            
+
             traeningBLL.AddTraening(valgtDato, valgtHold, alleFremmoederegistreringer);
 
             MessageBox.Show("Fremmøde registreret.");
